1b57510df4059aa92b93f5335a8a1ceffd43ca25
besated <bryan@seinetwork.io>
2023-02-08 16:53:21 -0800

    Create sei signing cosmwasm client

--- packages/core/src/lib/client/index.ts
+++ packages/core/src/lib/client/index.ts
@@ -1,4 +1,5 @@
 export * from './events';
 export * from './seiCosmWasmClient';
+export * from './seiSigningCosmWasmClient';
 export * from './seiSigningStargateClient';
 export * from './seiStargateClient';
