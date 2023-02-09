6070b4002dab670f4166cf18032c8a66f2c30cbd
besated <bryan@seinetwork.io>
2023-02-08 16:46:49 -0800

    Add sei cosmwasm client

--- packages/core/src/lib/client/index.ts
+++ packages/core/src/lib/client/index.ts
@@ -1,3 +1,4 @@
-export * from './seiStargateClient';
-export * from './seiSigningStargateClient';
 export * from './events';
+export * from './seiCosmWasmClient';
+export * from './seiSigningStargateClient';
+export * from './seiStargateClient';
