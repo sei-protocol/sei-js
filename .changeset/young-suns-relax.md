425683600296ba21a743e33ea096b360615efb78
besated <99624770+besated@users.noreply.github.com>
2023-02-08 18:04:00 -0800

    Extend CosmWasmClients for Sei Tendermint (#13)
    * Add sei cosmwasm client
    
    * Create sei signing cosmwasm client
    
    * Add changeset

---
+++ .changeset/young-suns-relax.md
@@ -0,0 +1,5 @@
+---
+'@sei-js/core': patch
+---
+
+Add SeiCosmWasmClient with tendermint 0.35 implementation
--- packages/core/src/lib/client/index.ts
+++ packages/core/src/lib/client/index.ts
@@ -1,3 +1,5 @@
-export * from './seiStargateClient';
-export * from './seiSigningStargateClient';
 export * from './events';
+export * from './seiCosmWasmClient';
+export * from './seiSigningCosmWasmClient';
+export * from './seiSigningStargateClient';
+export * from './seiStargateClient';
