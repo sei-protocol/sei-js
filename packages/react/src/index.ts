99b9a8166185de31e94fb7b060b08f3a4eb969cb
Carson Aberle <carson@pineapplelabs.io>
2023-08-30 23:02:33 -0700

    Fixed conditional issue

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,19 +1,11 @@
-// Polyfill process and buffer for browser
 import * as process from 'process';
 import { Buffer } from 'buffer';
 
-// Only polyfill if they don't exist
-if (typeof self.process === 'undefined') {
-	Object.assign(self, { process });
-}
-
-if (typeof self.Buffer === 'undefined') {
-	Object.assign(self, { Buffer });
-}
-
-// Existing global becomes globalThis
-if (typeof globalThis === 'undefined') {
-	Object.assign(self, { global: self });
-}
+// Polyfill process and buffer for browser
+Object.assign(self, {
+	process,
+	global: self,
+	Buffer
+});
 
 export * from './lib';
