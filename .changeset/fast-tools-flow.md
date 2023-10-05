8ee4b8e9637ebf62c25ba2779c8dc69d31a6980c
Carson <carson@seinetwork.io>
2023-10-05 09:52:33 -0700

    Adjusted babel files, removed proto/types from gitas it is generated,

--- .changeset/fast-tools-flow.md
+++
@@ -1,7 +0,0 @@
----
-'@sei-js/proto': patch
-'@sei-js/react': patch
-'@sei-js/core': patch
----
-
-Added build files for esm builds to enable tree shaking and other build optimizations
---
+++ packages/core/.babelrc.js
@@ -0,0 +1,24 @@
+module.exports = (api) => {
+	const babelEnv = process.env.BABEL_ENV || 'esm';
+
+	api.cache(() => babelEnv);
+
+	const isESM = babelEnv === 'esm';
+
+	const commonjsPresets = [
+		[
+			'@babel/preset-env',
+			{
+				modules: 'commonjs'
+			}
+		],
+		'@babel/preset-typescript'
+	];
+
+	const esmPresets = ['@babel/preset-env', '@babel/preset-typescript'];
+
+	return {
+		presets: isESM ? esmPresets : commonjsPresets,
+		plugins: ['@babel/plugin-transform-runtime']
+	};
+};
