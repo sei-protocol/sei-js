8d287e1553eec20aee2afe4282bdd621d6a2d45b
Carson <104383295+codebycarson@users.noreply.github.com>
2024-03-20 23:55:28 -0700

    fixed failing tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,13 @@
 import { getJestProjects } from '@nx/jest';
 
 export default {
-	projects: getJestProjects()
+	projects: getJestProjects(),
+	preset: 'ts-jest',
+	testMatch: ['**/*.spec.ts', '**/*.spec.tsx'],
+	globals: {
+		'ts-jest': {
+			tsconfig: './tsconfig.base.json'
+		}
+	},
+	modulePathIgnorePatterns: ['<rootDir>/packages/*/dist/']
 };
