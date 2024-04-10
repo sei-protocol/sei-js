fcebdac346d1f9c36221289382b12df0f8e84280
Carson <104383295+codebycarson@users.noreply.github.com>
2024-04-09 23:37:35 -0700

    Added chain id exports

--- packages/registry/src/index.ts
+++ packages/registry/src/index.ts
@@ -8,3 +8,12 @@ export * from './ibc';
  * A TypeScript type representing the official Sei network chain identifiers.
  */
 export type Network = 'pacific-1' | 'atlantic-2' | 'arctic-1';
+
+/**
+ * An object for referencing the official Sei network chain identifiers.
+ */
+export const CHAIN_IDS = {
+	mainnet: 'pacific-1',
+	testnet: 'atlantic-2',
+	devnet: 'arctic-1'
+};
