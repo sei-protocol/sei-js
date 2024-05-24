56a746fd736b65a221f7856ba51338039aea2718
besated <bryan@seinetwork.io>
2024-05-24 15:21:36 -0700

    Add pacific-1 evm chain config

--- packages/evm/src/chainInfo/__tests__/chainInfo.spec.ts
+++ packages/evm/src/chainInfo/__tests__/chainInfo.spec.ts
@@ -1,12 +1,12 @@
 // config.test.ts
-import { SeiChainInfo } from '../chainInfo';
+import { SEI_CHAIN_INFO } from '../chainInfo';
 
 describe('arctic-1 tests', () => {
 	test('CHAIN ID should be correct', () => {
-		expect(SeiChainInfo.devnet.chainId).toBe(713715);
+		expect(SEI_CHAIN_INFO.devnet.chainId).toBe(713715);
 	});
 
 	test('EVM RPC should be correct', () => {
-		expect(SeiChainInfo.devnet.defaultRpc).toBe('https://evm-rpc-arctic-1.sei-apis.com');
+		expect(SEI_CHAIN_INFO.devnet.defaultRpc).toBe('https://evm-rpc-arctic-1.sei-apis.com');
 	});
 });
--- packages/evm/src/providers/viem.ts
+++ packages/evm/src/providers/viem.ts
@@ -1,5 +1,5 @@
 import { Chain } from 'viem';
-import { SeiChainInfo } from '../chainInfo';
+import { SEI_CHAIN_INFO } from '../chainInfo';
 import {
 	ADDRESS_PRECOMPILE_ADDRESS,
 	BANK_PRECOMPILE_ADDRESS,
@@ -8,7 +8,7 @@ import {
 	JSON_PRECOMPILE_ADDRESS,
 	STAKING_PRECOMPILE_ADDRESS,
 	WASM_PRECOMPILE_ADDRESS,
-  IBC_PRECOMPILE_ADDRESS
+	IBC_PRECOMPILE_ADDRESS
 } from '../precompiles';
 
 /**
@@ -37,7 +37,7 @@ import {
 export const ARCTIC_1_VIEM_CHAIN: Chain = {
 	blockExplorers: {
 		default: {
-			name: 'SeiTrace',
+			name: 'Seitrace',
 			url: 'https://seitrace.com'
 		}
 	},
@@ -52,7 +52,7 @@ export const ARCTIC_1_VIEM_CHAIN: Chain = {
 		ibcPrecompile: { address: IBC_PRECOMPILE_ADDRESS }
 	},
 	fees: undefined,
-	id: SeiChainInfo.devnet.chainId,
+	id: SEI_CHAIN_INFO.devnet.chainId,
 	name: 'Sei Devnet',
 	nativeCurrency: {
 		name: 'Sei',
@@ -61,10 +61,10 @@ export const ARCTIC_1_VIEM_CHAIN: Chain = {
 	},
 	rpcUrls: {
 		default: {
-			http: [SeiChainInfo.devnet.defaultRpc]
+			http: [SEI_CHAIN_INFO.devnet.defaultRpc]
 		}
 	},
-	sourceId: SeiChainInfo.devnet.chainId,
+	sourceId: SEI_CHAIN_INFO.devnet.chainId,
 	testnet: true
 };
 
@@ -94,7 +94,7 @@ export const ARCTIC_1_VIEM_CHAIN: Chain = {
 export const ATLANTIC_2_VIEM_CHAIN: Chain = {
 	blockExplorers: {
 		default: {
-			name: 'SeiTrace',
+			name: 'Seitrace',
 			url: 'https://seitrace.com'
 		}
 	},
@@ -110,7 +110,7 @@ export const ATLANTIC_2_VIEM_CHAIN: Chain = {
 		ibcPrecompile: { address: IBC_PRECOMPILE_ADDRESS }
 	},
 	fees: undefined,
-	id: SeiChainInfo.testnet.chainId,
+	id: SEI_CHAIN_INFO.testnet.chainId,
 	name: 'Sei Testnet',
 	nativeCurrency: {
 		name: 'Sei',
@@ -119,9 +119,66 @@ export const ATLANTIC_2_VIEM_CHAIN: Chain = {
 	},
 	rpcUrls: {
 		default: {
-			http: [SeiChainInfo.testnet.defaultRpc]
+			http: [SEI_CHAIN_INFO.testnet.defaultRpc]
 		}
 	},
-	sourceId: SeiChainInfo.testnet.chainId,
+	sourceId: SEI_CHAIN_INFO.testnet.chainId,
+	testnet: true
+};
+
+/**
+ * Creates and returns a Viem Chain with the default pacific-1 configs and precompile contracts added.
+ * @example
+ * React: Use with `createWagmiConfig` for use in a WagmiProvider:
+ * ```tsx
+ * import { ReactNode } from 'react';
+ * import { WagmiProvider } from 'wagmi';
+ * import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+ * import { PACIFIC_1_VIEM_CHAIN, createWagmiConfig } from '@sei-js/evm';
+ *
+ * const queryClient = new QueryClient();
+ *
+ * export const WalletProvider = ({ children }: { children: ReactNode }) => {
+ *  return (
+ *    <WagmiProvider config={createWagmiConfig(PACIFIC_1_VIEM_CHAIN)}>
+ *      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+ *    </WagmiProvider>
+ *  );
+ * };
+ * ```
+ * @returns A 'viem' package chain compatible with Wagmi and Viem.
+ * @category Viem
+ */
+export const PACIFIC_1_VIEM_CHAIN: Chain = {
+	blockExplorers: {
+		default: {
+			name: 'Seitrace',
+			url: 'https://seitrace.com'
+		}
+	},
+	contracts: {
+		addressPrecompile: { address: ADDRESS_PRECOMPILE_ADDRESS },
+		bankPrecompile: { address: BANK_PRECOMPILE_ADDRESS },
+		distributionPrecompile: { address: DISTRIBUTION_PRECOMPILE_ADDRESS },
+		governancePrecompile: { address: GOVERNANCE_PRECOMPILE_ADDRESS },
+		jsonPrecompile: { address: JSON_PRECOMPILE_ADDRESS },
+		stakingPrecompile: { address: STAKING_PRECOMPILE_ADDRESS },
+		wasmPrecompile: { address: WASM_PRECOMPILE_ADDRESS },
+		ibcPrecompile: { address: IBC_PRECOMPILE_ADDRESS }
+	},
+	fees: undefined,
+	id: SEI_CHAIN_INFO.mainnet.chainId,
+	name: 'Sei',
+	nativeCurrency: {
+		name: 'Sei',
+		decimals: 18,
+		symbol: 'SEI'
+	},
+	rpcUrls: {
+		default: {
+			http: [SEI_CHAIN_INFO.mainnet.defaultRpc]
+		}
+	},
+	sourceId: SEI_CHAIN_INFO.mainnet.chainId,
 	testnet: true
 };
