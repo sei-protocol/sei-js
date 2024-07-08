3d65649979660bea78df692e89db73093b5dca99
mj <riku850@gmail.com>
2024-07-08 01:34:28 -0400

    update next wagmi template

--- packages/create-sei/templates/next-wagmi-template/src/app/page.tsx
+++ packages/create-sei/templates/next-wagmi-template/src/app/page.tsx
@@ -1,22 +1,21 @@
-import styles from "./page.module.css";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Web3Provider from "./components/Web3Provider";
 import Homepage from "./components/Homepage/Homepage";
 
+import './index.css'
+
 export default function Home() {
   return (
     <Web3Provider>
-      <div className={styles.appContainer}>
-        <div className={styles.header}>
-          <ConnectButton 
-              showBalance={{
-              smallScreen: false,
-              largeScreen: true,
-              }}
-            />
-        </div>
-        <Homepage/>
+      <div className="header">
+        <ConnectButton 
+            showBalance={{
+            smallScreen: false,
+            largeScreen: true,
+            }}
+          />
       </div>
+      <Homepage/>
     </Web3Provider>
   );
 }
