bef7117a039dbb414c3eb92b32d44611f5554bd6
mj <riku850@gmail.com>
2024-09-04 16:40:17 -0400

    precompile additions

--- packages/evm/src/precompiles/address.ts
+++ packages/evm/src/precompiles/address.ts
@@ -23,5 +23,19 @@ export const ADDRESS_PRECOMPILE_ABI = [
 		outputs: [{ internalType: 'string', name: 'response', type: 'string' }],
 		stateMutability: 'view',
 		type: 'function'
-	}
+	},
+	{
+		inputs: [{ internalType: 'string', name: 'v', type: 'string' }, { internalType: 'string', name: 'r', type: 'string' }, { internalType: 'string', name: 's', type: 'string' }, { internalType: 'string', name: 'customMessage', type: 'string' }],
+		name: 'associate',
+		outputs: [{ internalType: 'string', name: 'seiAddr', type: 'string' }, { internalType: 'address', name: 'evmAddr', type: 'address' }],
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		inputs: [{ internalType: 'string', name: 'pubKeyHex', type: 'string' }],
+		name: 'associatePubKey',
+		outputs: [{ internalType: 'string', name: 'seiAddr', type: 'string' }, { internalType: 'address', name: 'evmAddr', type: 'address' }],
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
 ] as const;
