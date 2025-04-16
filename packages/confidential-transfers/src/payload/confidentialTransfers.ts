fc929b160b9dcbdaf3a9b07476fdc9a49bf68676
Carson <104383295+codebycarson@users.noreply.github.com>
2025-04-16 13:39:58 -0700

    Got CT Building Properly
    - Removed "packageManager" field from confidential transfers package.json
    - Fixed name of package for confidential transfers
    - Biome reformatting and typescript fixes
    - Switched from vite to tsc for consistency with other packages

--- packages/confidential-transfers/src/payload/confidentialTransfers.ts
+++ packages/confidential-transfers/src/payload/confidentialTransfers.ts
@@ -1,267 +1,246 @@
-import {  MsgApplyPendingBalance, MsgCloseAccount, MsgInitializeAccount, MsgWithdraw, MsgTransfer } from "@sei-js/cosmos/types/confidentialtransfers"
 import { Encoder } from '@sei-js/cosmos/encoding';
+import type { MsgApplyPendingBalance, MsgCloseAccount, MsgInitializeAccount, MsgTransfer, MsgWithdraw } from '@sei-js/cosmos/types/confidentialtransfers';
 
-import { ctWasmBase64 } from './embeddedWasm';
 import { base64FromBytes, base64ToBytes } from '../utils/utils';
+import { ctWasmBase64 } from './embeddedWasm';
 
-const global = globalThis as any;
+const global = globalThis;
 
 export class ConfidentialTransfers {
-  private go: any;
-  private wasmInstance: WebAssembly.Instance | null = null;
-  public initialized: boolean = false
+	private go: {
+		run: (instance: WebAssembly.Instance) => void;
+		importObject: WebAssembly.Imports | undefined;
+	};
+	private wasmInstance: WebAssembly.Instance | null = null;
+	public initialized = false;
 
-  // This constructor runs the wasm_exec script to load the wasm methods into the global object.
-  // This enables us to run Go generated wasm code.
-  constructor() {
-    // Dynamically import wasm_exec.js
-    if (typeof global.Go === "undefined") {
-      const script = document.createElement("script");
-      script.src = "./wasm_exec.cjs"; // Adjust path based on your environment
-      document.head.appendChild(script);
-    }
+	// This constructor runs the wasm_exec script to load the wasm methods into the global object.
+	// This enables us to run Go generated wasm code.
+	constructor() {
+		// Dynamically import wasm_exec.js
+		if (typeof global.Go === 'undefined') {
+			const script = document.createElement('script');
+			script.src = './wasm_exec.cjs'; // Adjust path based on your environment
+			document.head.appendChild(script);
+		}
 
-    // Wait until wasm_exec.cjs is loaded
-    if (typeof global.Go === "undefined") {
-      throw new Error("wasm_exec.cjs was not loaded correctly.");
-    }
+		// Wait until wasm_exec.cjs is loaded
+		if (typeof global.Go === 'undefined') {
+			throw new Error('wasm_exec.cjs was not loaded correctly.');
+		}
 
-    this.go = new global.Go();
-  }
+		this.go = new global.Go();
+	}
 
-  // Initialize loads the compiled and encoded wasm file into the object.
-  async initialize(): Promise<void> {
-    if (this.initialized) {
-      return
-    }
-    const wasmBytes = base64ToBytes(ctWasmBase64.trim());
-    const { instance } = await WebAssembly.instantiate(wasmBytes, this.go.importObject);
+	// Initialize loads the compiled and encoded wasm file into the object.
+	async initialize(): Promise<void> {
+		if (this.initialized) {
+			return;
+		}
+		const wasmBytes = base64ToBytes(ctWasmBase64.trim());
+		const { instance } = await WebAssembly.instantiate(wasmBytes, this.go.importObject);
 
-    if (typeof this.go.run !== "function") {
-      console.error("Go runtime's 'run' is not a function. Current Go object:", this.go);
-      throw new Error("Go runtime is not properly initialized.");
-    }
+		if (typeof this.go.run !== 'function') {
+			console.error("Go runtime's 'run' is not a function. Current Go object:", this.go);
+			throw new Error('Go runtime is not properly initialized.');
+		}
 
-    this.wasmInstance = instance;
-    this.go.run(instance);
-    this.initialized = true;
-  }
+		this.wasmInstance = instance;
+		this.go.run(instance);
+		this.initialized = true;
+	}
 
-  createWithdraw(
-    signedDenom: Uint8Array,
-    amount: number,
-    address: string,
-    denom: string,
-    currentDecryptableBalance: string,
-    currentAvailableBalance: Uint8Array
-  ): MsgWithdraw {
-    if (!this.wasmInstance) {
-      throw new Error("WASM module not initialized");
-    }
+	createWithdraw(
+		signedDenom: Uint8Array,
+		amount: number,
+		address: string,
+		denom: string,
+		currentDecryptableBalance: string,
+		currentAvailableBalance: Uint8Array
+	): MsgWithdraw {
+		if (!this.wasmInstance) {
+			throw new Error('WASM module not initialized');
+		}
 
-    const signedDenomBase64 = Buffer.from(signedDenom).toString("base64");
-    const availableBalanceBase64 = Buffer.from(currentAvailableBalance).toString("base64");
+		const signedDenomBase64 = Buffer.from(signedDenom).toString('base64');
+		const availableBalanceBase64 = Buffer.from(currentAvailableBalance).toString('base64');
 
-    if (!global.createWithdrawStruct) {
-      throw new Error("createWithdrawStruct is not available in the WebAssembly global scope.");
-    }
+		if (!global.createWithdrawStruct) {
+			throw new Error('createWithdrawStruct is not available in the WebAssembly global scope.');
+		}
 
-    const withdrawStruct = global.createWithdrawStruct(
-      signedDenomBase64,
-      amount.toString(),
-      address,
-      denom,
-      currentDecryptableBalance,
-      availableBalanceBase64);
+		const withdrawStruct = global.createWithdrawStruct(signedDenomBase64, amount.toString(), address, denom, currentDecryptableBalance, availableBalanceBase64);
 
-    const withdrawStructJson = JSON.parse(withdrawStruct);
-    const result = Encoder.confidentialtransfers.MsgWithdraw.fromJSON(withdrawStructJson)
+		const withdrawStructJson = JSON.parse(withdrawStruct);
+		const result = Encoder.confidentialtransfers.MsgWithdraw.fromJSON(withdrawStructJson);
 
-    return result
-  }
+		return result;
+	}
 
-  createInitialize(
-    signedDenom: Uint8Array,
-    address: string,
-    denom: string,
-  ): MsgInitializeAccount {
-    if (!this.wasmInstance) {
-      throw new Error("WASM module not initialized");
-    }
+	createInitialize(signedDenom: Uint8Array, address: string, denom: string): MsgInitializeAccount {
+		if (!this.wasmInstance) {
+			throw new Error('WASM module not initialized');
+		}
 
-    const signedDenomBase64 = base64FromBytes(signedDenom)
+		const signedDenomBase64 = base64FromBytes(signedDenom);
 
-    if (!global.createInitializeStruct) {
-      throw new Error("createInitializeStruct is not available in the WebAssembly global scope.");
-    }
+		if (!global.createInitializeStruct) {
+			throw new Error('createInitializeStruct is not available in the WebAssembly global scope.');
+		}
 
-    const initializeStruct = global.createInitializeStruct(
-      signedDenomBase64,
-      address,
-      denom,
-    )
+		const initializeStruct = global.createInitializeStruct(signedDenomBase64, address, denom);
 
-    const initializeStructJson = JSON.parse(initializeStruct);
-    const result = Encoder.confidentialtransfers.MsgInitializeAccount.fromJSON(initializeStructJson)
+		const initializeStructJson = JSON.parse(initializeStruct);
+		const result = Encoder.confidentialtransfers.MsgInitializeAccount.fromJSON(initializeStructJson);
 
-    // 2) Cast (type assertion) at compile-time
-    return result
-  }
+		// 2) Cast (type assertion) at compile-time
+		return result;
+	}
 
-  createApplyPendingBalance(
-    signedDenom: Uint8Array,
-    address: string,
-    denom: string,
-    pendingBalanceCreditCounter: number,
-    currentDecryptableBalance: string,
-    currentAvailableBalance: Uint8Array,
-    currentPendingBalanceLo: Uint8Array,
-    currentPendingBalanceHi: Uint8Array,
-  ): MsgApplyPendingBalance {
-    if (!this.wasmInstance) {
-      throw new Error("WASM module not initialized");
-    }
-    
-    const signedDenomBase64 = base64FromBytes(signedDenom);
-    const availableBalanceBase64 = base64FromBytes(currentAvailableBalance);
-    const pendingBalanceLoBase64 = base64FromBytes(currentPendingBalanceLo);
-    const pendingBalanceHiBase64 = base64FromBytes(currentPendingBalanceHi);
+	createApplyPendingBalance(
+		signedDenom: Uint8Array,
+		address: string,
+		denom: string,
+		pendingBalanceCreditCounter: number,
+		currentDecryptableBalance: string,
+		currentAvailableBalance: Uint8Array,
+		currentPendingBalanceLo: Uint8Array,
+		currentPendingBalanceHi: Uint8Array
+	): MsgApplyPendingBalance {
+		if (!this.wasmInstance) {
+			throw new Error('WASM module not initialized');
+		}
 
-    if (!global.createApplyPendingBalanceStruct) {
-      throw new Error("createApplyPendingBalanceStruct is not available in the WebAssembly global scope.");
-    }
+		const signedDenomBase64 = base64FromBytes(signedDenom);
+		const availableBalanceBase64 = base64FromBytes(currentAvailableBalance);
+		const pendingBalanceLoBase64 = base64FromBytes(currentPendingBalanceLo);
+		const pendingBalanceHiBase64 = base64FromBytes(currentPendingBalanceHi);
 
-    const applyPendingBalanceStruct = global.createApplyPendingBalanceStruct(
-        signedDenomBase64,
-        address,
-        denom,
-        currentDecryptableBalance,
-        pendingBalanceCreditCounter,
-        availableBalanceBase64,
-        pendingBalanceLoBase64,
-        pendingBalanceHiBase64);
-    
-    const applyPendingBalanceStructJson = JSON.parse(applyPendingBalanceStruct);
+		if (!global.createApplyPendingBalanceStruct) {
+			throw new Error('createApplyPendingBalanceStruct is not available in the WebAssembly global scope.');
+		}
 
-    const result = Encoder.confidentialtransfers.MsgApplyPendingBalance.fromJSON(applyPendingBalanceStructJson)
+		const applyPendingBalanceStruct = global.createApplyPendingBalanceStruct(
+			signedDenomBase64,
+			address,
+			denom,
+			currentDecryptableBalance,
+			pendingBalanceCreditCounter,
+			availableBalanceBase64,
+			pendingBalanceLoBase64,
+			pendingBalanceHiBase64
+		);
 
-    return result
-  }
+		const applyPendingBalanceStructJson = JSON.parse(applyPendingBalanceStruct);
 
-  createTransfer(
-    signedDenom: Uint8Array,
-    amount: number,
-    senderAddress: string,
-    recipientAddress: string,
-    denom: string,
-    currentDecryptableBalance: string,
-    currentAvailableBalance: Uint8Array,
-    recipientPubkey: Uint8Array,
-  ): MsgTransfer {
-    if (!this.wasmInstance) {
-      throw new Error("WASM module not initialized");
-    }
+		const result = Encoder.confidentialtransfers.MsgApplyPendingBalance.fromJSON(applyPendingBalanceStructJson);
 
-    const signedDenomBase64 = base64FromBytes(signedDenom);
-    const availableBalanceBase64 = base64FromBytes(currentAvailableBalance);
-    const recipientPubkeyBase64 = base64FromBytes(recipientPubkey);
+		return result;
+	}
 
-    if (!global.createTransferStruct) {
-      throw new Error("createTransferStruct is not available in the WebAssembly global scope.");
-    }
+	createTransfer(
+		signedDenom: Uint8Array,
+		amount: number,
+		senderAddress: string,
+		recipientAddress: string,
+		denom: string,
+		currentDecryptableBalance: string,
+		currentAvailableBalance: Uint8Array,
+		recipientPubkey: Uint8Array
+	): MsgTransfer {
+		if (!this.wasmInstance) {
+			throw new Error('WASM module not initialized');
+		}
 
-    const transferStruct = global.createTransferStruct(
-      signedDenomBase64,
-      amount,
-      senderAddress,
-      recipientAddress,
-      denom,
-      currentDecryptableBalance,
-      availableBalanceBase64,
-      recipientPubkeyBase64);
+		const signedDenomBase64 = base64FromBytes(signedDenom);
+		const availableBalanceBase64 = base64FromBytes(currentAvailableBalance);
+		const recipientPubkeyBase64 = base64FromBytes(recipientPubkey);
 
-    const transferStructJson = JSON.parse(transferStruct);
-    const result = Encoder.confidentialtransfers.MsgTransfer.fromJSON(transferStructJson)
+		if (!global.createTransferStruct) {
+			throw new Error('createTransferStruct is not available in the WebAssembly global scope.');
+		}
 
-    return result
-  }
+		const transferStruct = global.createTransferStruct(
+			signedDenomBase64,
+			amount,
+			senderAddress,
+			recipientAddress,
+			denom,
+			currentDecryptableBalance,
+			availableBalanceBase64,
+			recipientPubkeyBase64
+		);
 
-  createCloseAccount(
-    signedDenom: Uint8Array,
-    address: string,
-    denom: string,
-    currentAvailableBalance: Uint8Array,
-    currentPendingBalanceLo: Uint8Array,
-    currentPendingBalanceHi: Uint8Array,
-  ): MsgCloseAccount {
-    if (!this.wasmInstance) {
-      throw new Error("WASM module not initialized");
-    }
+		const transferStructJson = JSON.parse(transferStruct);
+		const result = Encoder.confidentialtransfers.MsgTransfer.fromJSON(transferStructJson);
 
-    const signedDenomBase64 = base64FromBytes(signedDenom);
-    const availableBalanceBase64 = base64FromBytes(currentAvailableBalance);
-    const pendingBalanceLoBase64 = base64FromBytes(currentPendingBalanceLo);
-    const pendingBalanceHiBase64 = base64FromBytes(currentPendingBalanceHi);
+		return result;
+	}
 
-    if (!global.createCloseAccountStruct) {
-      throw new Error("createCloseAccountStruct is not available in the WebAssembly global scope.");
-    }
+	createCloseAccount(
+		signedDenom: Uint8Array,
+		address: string,
+		denom: string,
+		currentAvailableBalance: Uint8Array,
+		currentPendingBalanceLo: Uint8Array,
+		currentPendingBalanceHi: Uint8Array
+	): MsgCloseAccount {
+		if (!this.wasmInstance) {
+			throw new Error('WASM module not initialized');
+		}
 
-    const closeAccountStruct = global.createCloseAccountStruct(
-      signedDenomBase64,
-      address,
-      denom,
-      pendingBalanceLoBase64,
-      pendingBalanceHiBase64,
-      availableBalanceBase64,
-    );
+		const signedDenomBase64 = base64FromBytes(signedDenom);
+		const availableBalanceBase64 = base64FromBytes(currentAvailableBalance);
+		const pendingBalanceLoBase64 = base64FromBytes(currentPendingBalanceLo);
+		const pendingBalanceHiBase64 = base64FromBytes(currentPendingBalanceHi);
 
-    const closeAccountStructJson = JSON.parse(closeAccountStruct);
-    const result = Encoder.confidentialtransfers.MsgCloseAccount.fromJSON(closeAccountStructJson)
+		if (!global.createCloseAccountStruct) {
+			throw new Error('createCloseAccountStruct is not available in the WebAssembly global scope.');
+		}
 
-    return result
-  }
+		const closeAccountStruct = global.createCloseAccountStruct(
+			signedDenomBase64,
+			address,
+			denom,
+			pendingBalanceLoBase64,
+			pendingBalanceHiBase64,
+			availableBalanceBase64
+		);
 
-  decryptCiphertext(
-    signedDenom: Uint8Array,
-    ciphertext: Uint8Array
-  ): bigint {
-    if (!this.wasmInstance) {
-      throw new Error("WASM module not initialized");
-    }
+		const closeAccountStructJson = JSON.parse(closeAccountStruct);
+		const result = Encoder.confidentialtransfers.MsgCloseAccount.fromJSON(closeAccountStructJson);
 
-    const signedDenomBase64 = base64FromBytes(signedDenom);
-    const ciphertextBase64 = base64FromBytes(ciphertext);
+		return result;
+	}
 
-    if (!global.decryptCiphertext) {
-      throw new Error("decryptCiphertext is not available in the WebAssembly global scope.");
-    }
-    const decryptedCiphertext = global.decryptCiphertext(
-      signedDenomBase64,
-      ciphertextBase64
-    );
+	decryptCiphertext(signedDenom: Uint8Array, ciphertext: Uint8Array): bigint {
+		if (!this.wasmInstance) {
+			throw new Error('WASM module not initialized');
+		}
 
-    return BigInt(decryptedCiphertext)
-  }
+		const signedDenomBase64 = base64FromBytes(signedDenom);
+		const ciphertextBase64 = base64FromBytes(ciphertext);
 
-  decryptAESCiphertext(
-    signedDenom: Uint8Array,
-    aesCiphertext: string
-  ): bigint {
-    if (!this.wasmInstance) {
-      throw new Error("WASM module not initialized");
-    }
+		if (!global.decryptCiphertext) {
+			throw new Error('decryptCiphertext is not available in the WebAssembly global scope.');
+		}
+		const decryptedCiphertext = global.decryptCiphertext(signedDenomBase64, ciphertextBase64);
 
-    const signedDenomBase64 = base64FromBytes(signedDenom);
+		return BigInt(decryptedCiphertext);
+	}
 
-    if (!global.decryptAesCiphertext) {
-      throw new Error("decryptAesCiphertext is not available in the WebAssembly global scope.");
-    }
-    const decryptedCiphertext = global.decryptAesCiphertext(
-      signedDenomBase64,
-      aesCiphertext
-    );
+	decryptAESCiphertext(signedDenom: Uint8Array, aesCiphertext: string): bigint {
+		if (!this.wasmInstance) {
+			throw new Error('WASM module not initialized');
+		}
 
-    return BigInt(decryptedCiphertext)
-  }
+		const signedDenomBase64 = base64FromBytes(signedDenom);
+
+		if (!global.decryptAesCiphertext) {
+			throw new Error('decryptAesCiphertext is not available in the WebAssembly global scope.');
+		}
+		const decryptedCiphertext = global.decryptAesCiphertext(signedDenomBase64, aesCiphertext);
+
+		return BigInt(decryptedCiphertext);
+	}
 }
