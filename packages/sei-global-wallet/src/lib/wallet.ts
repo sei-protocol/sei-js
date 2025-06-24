55d1a967e895398c91c1667c131be5c6620426c1
carson <carson.aberle+codebycarson@gmail.com>
2025-06-24 13:40:00 -0600

    Fix biome for /sei-global-wallet

--- packages/sei-global-wallet/src/lib/wallet.ts
+++ packages/sei-global-wallet/src/lib/wallet.ts
@@ -1,4 +1,4 @@
-import { createGlobalWalletClient, GlobalWalletClient } from '@dynamic-labs/global-wallet-client';
+import { type GlobalWalletClient, createGlobalWalletClient } from '@dynamic-labs/global-wallet-client';
 import { config } from './config';
 
 const Wallet: GlobalWalletClient = createGlobalWalletClient({
